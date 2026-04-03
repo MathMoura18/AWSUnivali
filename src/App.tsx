eca599e0d1f8e1097fc35cd36e35c0ba5f8a19de
Matheus Moura <mathmo.ura242@gmail.com>
2026-04-03 15:54:01 -0300

    basename no react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/Sobre'; // O componente que criamos antes
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/AWSUnivali/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<About />} />
